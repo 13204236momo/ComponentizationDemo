9706c62d885e164d3e7746c551befddf1c4932e4
zhou <953605663@qq.com>
2019-08-09 11:26:01 +0800

    组件化部署完成

---
+++ order/src/main/java/com/example/componentizationdemo/order/debug/Order_DebugMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.componentizationdemo.order.debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.componentizationdemo.order.R;
+
+public class Order_DebugMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_debug_main_activity);
+    }
+}
